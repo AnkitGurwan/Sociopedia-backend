5c819b8ceb9f3f4fb57ec898890a06ba40a1c5f0
AnkitGurwan <ankitgurwan083@gmail.com>
2023-06-10 17:27:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import database from "./config/database.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
+import { createPost } from "./controllers/post.js";
+import { register } from "./controllers/auth.js";
+import { verifytoken } from "./middleware/auth.js";
+
+
+// Configurations 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({path:"config/.env"});
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit : "30mb" , extended : true}));
+app.use(bodyParser.urlencoded({limit : "30mb" , extended : true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname, 'public/assets')));
+
+// multer configure 
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"./public/assets");
+    },
+    filename: function(req,file,cb){
+        cb(null,file.originalname)
+    }
+});
+const upload = multer({storage});
+
+//Routes for resister with image
+app.post("/auth/register",upload.single("picture"),register);
+app.post("/posts",upload.single("picture"),createPost);
+
+//Routes
+app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
+app.use("/posts",postRoutes);
+
+//mongoose connection
+database();
+
+//connection
+app.listen(process.env.PORT,(req,res,err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log(`Server is listening on port : ${process.env.PORT}`)
+})
---
+++ routes/post.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { likePost , getUserPosts , getFeedPosts } from "../controllers/post.js";
+import { verifytoken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/all" , getFeedPosts);
+router.get("/:id/posts" , getUserPosts);
+
+router.patch("/:id/likepost"  , likePost);
+
+export default router;
